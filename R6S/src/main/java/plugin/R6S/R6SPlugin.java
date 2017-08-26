541b5971220ec5f1d267fa7113b12a89c6762410
BuildTools <unconfigured@null.spigotmc.org>
2017-08-26 16:10:11 +0900

    removed unnecessary code

--- R6S/src/main/java/plugin/R6S/R6SPlugin.java
+++ R6S/src/main/java/plugin/R6S/R6SPlugin.java
@@ -1,5 +1,7 @@
 package plugin.R6S;
 
+import java.io.File;
+
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,4 +14,36 @@ public class R6SPlugin extends JavaPlugin implements Listener{
 	public static R6SPlugin getInstance() {
 		return instance;
 	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	@Override
+	public void onEnable() {
+
+		// --check if datafolder and config.yml exist--
+		try {
+			if (!getDataFolder().exists()) {
+				getLogger().info("datafolder is not found, creating...");
+				getDataFolder().mkdirs();
+			}
+			if (!new File(getDataFolder(), "config.yml").exists()) {
+				getLogger().info("config.yml is not found, creating...");
+				saveDefaultConfig();
+			}
+			else {
+				getLogger().info("config.yml is found, loading...");
+			}
+			File devfile = new File(getDataFolder(), "devfile.yml");
+			if (!devfile.exists()) {
+				getLogger().info("devfile.yml is not found, creating...");
+				devfile.createNewFile();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
