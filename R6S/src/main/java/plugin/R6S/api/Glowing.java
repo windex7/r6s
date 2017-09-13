1fe2daffc98395615c6f34fc7da9dd9b68d40302
windex7 <w1ndex0721@gmail.com>
2017-09-14 02:43:03 +0900

    added glowing and changed set commiter signature

--- R6S/src/main/java/plugin/R6S/api/Glowing.java
+++ R6S/src/main/java/plugin/R6S/api/Glowing.java
@@ -1,5 +1,13 @@
 package plugin.R6S.api;
 
-public class Glowing {
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
+public class Glowing {
+	public static void setPlayerGlowing(Player player) {
+		if (player != null) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 200, 1));
+		}
+	}
 }
